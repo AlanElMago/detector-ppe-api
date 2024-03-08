9cb077dc729695acc733783992ab2ffe5663502a
Alan Franco <alanfrancob97@gmail.com>
2024-03-08 16:17:16 -0600

    Modified WhatsAppApiSettings.cs Fixed formatting.

--- DetectorPpeApi/Models/WhatsAppApiSettings.cs
+++ DetectorPpeApi/Models/WhatsAppApiSettings.cs
@@ -3,8 +3,12 @@
 public class WhatsAppApiSettings
 {
     public string? AccessToken { get; init; }
+
     public string? ApiUrl { get; init; }
-    public string? PhoneNumberId { get; init; }
+
     public string? ApiVersion { get; init; }
+
+    public string? PhoneNumberId { get; init; }
+
     public string? RequestUri => $"{ApiUrl}/v{ApiVersion}";
 }
