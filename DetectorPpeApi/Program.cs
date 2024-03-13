b0d9188da2638c9cebb82fff1905f6669ba04f69
Alan Franco <alanfrancob97@gmail.com>
2024-03-13 16:38:23 -0600

    Added Azure Key Vault Dependencies Added dependencies for the Azure Key Vault services. Connection to Azure Key vault is not yet implemented.

--- DetectorPpeApi/Program.cs
+++ DetectorPpeApi/Program.cs
@@ -2,8 +2,22 @@ using DetectorPpeApi.Authentication;
 using DetectorPpeApi.Models;
 using DetectorPpeApi.Services;
 using Microsoft.OpenApi.Models;
+using Azure.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
+
+string vaultUri = Environment.GetEnvironmentVariable("VaultUri")
+    ?? throw new InvalidOperationException("Vault URI not found.");
+
+Uri keyVaultEndpoint = new(vaultUri);
+builder.Configuration.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
+
+string authApikey = builder.Configuration["Authentication-ApiKey"]
+    ?? throw new InvalidOperationException("API Key not found.");
+string whatsAppApiAccessToken = builder.Configuration["WhatsAppApi-AccessToken"]
+    ?? throw new InvalidOperationException("Access Token not found.");
+string whatsAppApiPhoneNumberId = builder.Configuration["WhatsAppApi-PhoneNumberId"]
+    ?? throw new InvalidOperationException("Phone Number ID not found.");
 
 // Load configuration.
 builder.Services.Configure<WhatsAppApiSettings>(builder.Configuration.GetSection("WhatsAppApi"));
