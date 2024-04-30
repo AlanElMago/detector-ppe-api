bf9af76566675237d89a19a66c15487ddbcf278d
Alan Franco <alanfrancob97@gmail.com>
2024-04-30 10:30:21 -0600

    Modified WeatherForecastController.cs Removed SendToWhatsApp Post method used for tesing purposes.

--- DetectorPpeApi/Controllers/WeatherForecastController.cs
+++ DetectorPpeApi/Controllers/WeatherForecastController.cs
@@ -1,23 +1,15 @@
-using DetectorPpeApi.Authentication;
-using DetectorPpeApi.Services;
 using Microsoft.AspNetCore.Mvc;
-using System.Text.Json.Nodes;
 
 namespace DetectorPpeApi.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class WeatherForecastController(
-    IWhatsAppService whatsAppService,
-    ILogger<WeatherForecastController> logger) : ControllerBase
+public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries =
     [
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     ];
-
-    private readonly ILogger<WeatherForecastController> _logger = logger;
-    private readonly IWhatsAppService _whatsAppService = whatsAppService;
 
     [HttpGet]
     public IEnumerable<WeatherForecast> Get()
@@ -29,24 +21,5 @@ public class WeatherForecastController(
             Summary = Summaries[Random.Shared.Next(Summaries.Length)]
         })
         .ToArray();
-    }
-
-    [HttpPost("SendToWhatsApp/{recipientPhoneNumber}")]
-    [ServiceFilter(typeof(ApiKeyAuthFilter))]
-    public async Task<IActionResult> SendWeatherForcastToWhatsApp([FromRoute] string recipientPhoneNumber)
-    {
-        if (string.IsNullOrEmpty(recipientPhoneNumber))
-        {
-            return BadRequest("Recipient phone number is required.");
-        }
-
-        string date = DateOnly.FromDateTime(DateTime.Now).ToString("d");
-        string temperature = Random.Shared.Next(-20, 55).ToString();
-        string summary = Summaries[Random.Shared.Next(Summaries.Length)];
-
-        string msg = $"Weather forecast for {date}: {temperature}°C and {summary}";
-        string res = await _whatsAppService.SendMessageAsync(msg, recipientPhoneNumber);
-
-        return Ok(res);
     }
 }
