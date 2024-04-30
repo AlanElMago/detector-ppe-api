2dd23f3635a656127986a3f3a3ec831defaeb684
Alan Franco <alanfrancob97@gmail.com>
2024-04-30 10:40:19 -0600

    Clean up WhatsAppService.cs Refactorized and cleaned up code in WhatsAppService.cs

--- DetectorPpeApi/Services/WhatsAppService.cs
+++ DetectorPpeApi/Services/WhatsAppService.cs
@@ -11,8 +11,9 @@ namespace DetectorPpeApi.Services;
 public interface IWhatsAppService
 {
     Task<string> SendMessageAsync(string msg, string recipientPhoneNumber);
-    Task<string> UploadMediaAsync(string filePath);
+
     Task<string> UploadMediaAsync(IFormFile img);
+
     Task<string> SendMediaMessageAsync(string mediaId, string recipientPhoneNumber);
 }
 
@@ -48,26 +49,6 @@ public class WhatsAppService : IWhatsAppService
         HttpResponseMessage res = await _httpClient.SendAsync(httpRequestMsg);
 
         return await res.Content.ReadAsStringAsync();
-    }
-
-    public async Task<string> UploadMediaAsync(string filePath)
-    {
-        StreamContent fileStreamContent = new(File.OpenRead(filePath));
-        fileStreamContent.Headers.ContentType = new MediaTypeHeaderValue(MediaTypeNames.Image.Jpeg);
-
-        HttpRequestMessage httpRequestMessage = new(HttpMethod.Post, $"{RequestUri}/{PhoneNumberId}/media");
-        httpRequestMessage.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue(MediaTypeNames.Multipart.FormData));
-        httpRequestMessage.Content = new MultipartFormDataContent
-        {
-            { fileStreamContent, "file", "sample.jpg" },
-            { new StringContent(MediaTypeNames.Image.Jpeg), "type" },
-            { new StringContent("whatsapp"), "messaging_product" }
-        };
-
-        HttpResponseMessage res = await _httpClient.SendAsync(httpRequestMessage);
-        string resContent = await res.Content.ReadAsStringAsync();
-
-        return JsonSerializer.Deserialize<UploadMediaResponse>(resContent)?.Id ?? string.Empty;
     }
 
     public async Task<string> UploadMediaAsync(IFormFile img)
